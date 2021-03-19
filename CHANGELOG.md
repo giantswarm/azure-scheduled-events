fbb7ad6dd3249278e716e8972ed6c816af4bf718
Christian Bianchi <christian@giantswarm.io>
2021-03-19 16:40:17 +0100

    Eviction fix (#26)
    * Ensure to wait long enough when draining a node before considering the node drained.
    
    * Change drain timeout to 15 minutes.

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -7,6 +7,14 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 
 ## [Unreleased]
 
+### Fixed
+
+- Ensure to wait long enough when draining a node before considering the node drained.
+
+### Changed
+
+- Change drain timeout to 15 minutes.
+
 ## [0.2.2] - 2021-02-23
 
 ### Fixed
--- pkg/eventhandler/drainer/drain.go
+++ pkg/eventhandler/drainer/drain.go
@@ -92,7 +92,10 @@ func (s *DrainEventHandler) evictPods(ctx context.Context, k8sclient kubernetes.
 			}
 		}
 
-		evicted := 0
+		left := len(customPods) + len(kubesystemPods)
+		if left == 0 {
+			return nil
+		}
 
 		if len(customPods) > 0 {
 			for _, pod := range customPods {
@@ -102,8 +105,6 @@ func (s *DrainEventHandler) evictPods(ctx context.Context, k8sclient kubernetes.
 				} else if err != nil {
 					return microerror.Mask(err)
 				}
-
-				evicted += 1
 			}
 		}
 
@@ -115,19 +116,13 @@ func (s *DrainEventHandler) evictPods(ctx context.Context, k8sclient kubernetes.
 				} else if err != nil {
 					return microerror.Mask(err)
 				}
-
-				evicted += 1
 			}
 		}
 
-		if evicted > 0 {
-			return microerror.Maskf(evictionInProgressError, "%d pods still pending eviction, waiting", evicted)
-		}
-
-		return nil
+		return microerror.Maskf(evictionInProgressError, "%d pods still pending eviction, waiting", left)
 	}
 
-	err := backoff.RetryNotify(o, backoff.NewConstant(5*time.Minute, 10*time.Second), backoff.NewNotifier(s.Logger, ctx))
+	err := backoff.RetryNotify(o, backoff.NewConstant(15*time.Minute, 10*time.Second), backoff.NewNotifier(s.Logger, ctx))
 	if err != nil {
 		return microerror.Mask(err)
 	}
